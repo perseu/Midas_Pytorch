0f582d1ae98a05f1c06af68369defbaeb00d9443
João Águas <jmsaguas@gmail.com>
2024-09-24 20:52:20 +0200

    Converting columns from Object to Numeric.

--- midas_pytorch.py
+++ midas_pytorch.py
@@ -12,3 +12,12 @@ import pandas as pd
 
 
 
+df = pd.read_csv('LoansDatasest.csv')
+
+# Converting relevant columns from object to numeric
+df['customer_income']=df['customer_income'].str.replace(',','').str.replace('£','')
+df['customer_income'] = pd.to_numeric(df['customer_income'], errors='coerce')
+
+df['loan_amnt']=df['loan_amnt'].str.replace(',','').str.replace('£','')
+df['loan_amnt'] = pd.to_numeric(df['loan_amnt'], errors='coerce')
+
